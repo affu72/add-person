e5ef76cadf1cdf10d5cbf19d2d8b2c902651c365
Afzal Nomani <afzalnomani1@gmail.com>
2023-01-14 01:17:11 +0530

    error modal implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import PersonList from "./Componens/PersonList";
+import ErrorModel from "./Componens/styled-compoent/ErrorModel";
+import UserForm from "./Componens/UserForm";
 
 function App() {
+  const [person, setPerson] = useState([]);
+
+  const formDataHandler = function (data) {
+    setPerson((prev) => [data, ...prev]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <UserForm getDataOnSubmit={formDataHandler} />;
+      <PersonList personData={person} />
     </div>
   );
 }
-
 export default App;
